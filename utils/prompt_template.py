7e4ba72965787257b58ccfcfa0b3d3fdba7e534d
yubraaj11 <yubrajsigdel1@gmail.com>
2024-10-19 08:38:12 +0545

    Template added

--- utils/prompt_template.py
+++ utils/prompt_template.py
@@ -1,5 +1,11 @@
-
-
 class PromptTemplate:
     def __init__(self):
         pass
+    chat_prompt = f"""
+                    You are an assistant proficient in providing detailed and concise results based on the given user q
+                    uery and the relevant documents: \n\n
+                    query: {query} \n\n
+                    relevant document source: {response}
+    """
+
+
